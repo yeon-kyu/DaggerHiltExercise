b36ef9498934735dc6ad0b2b1bcd7acaa70f5799
yeon-kyu <45001093+yeon-kyu@users.noreply.github.com>
2021-07-05 02:31:02 +0900

    Add MainViewModel

--- app/src/main/java/com/yeonkyu/daggerhiltexercise/MainActivity.kt
+++ app/src/main/java/com/yeonkyu/daggerhiltexercise/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.yeonkyu.daggerhiltexercise
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yeonkyu.daggerhiltexercise.viewModel.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+import androidx.activity.viewModels
+import androidx.annotation.VisibleForTesting
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject lateinit var MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel.fetchTrackList()
     }
 }
---
+++ app/src/main/java/com/yeonkyu/daggerhiltexercise/viewModel/MainViewModel.kt
@@ -0,0 +1,28 @@
+package com.yeonkyu.daggerhiltexercise.viewModel
+
+import android.util.Log
+import androidx.hilt.Assisted
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.yeonkyu.daggerhiltexercise.repository.MainRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+
+@HiltViewModel
+class MainViewModel @ViewModelInject constructor(
+    private val mainRepository: MainRepository,
+    @Assisted private val savedStateHandle: SavedStateHandle
+): ViewModel(){
+    fun fetchTrackList() {
+        viewModelScope.launch {
+            val response = mainRepository.fetchTrackList("greenday","song",20,0)
+            Log.e("DH_CHECK","response : $response")
+        }
+
+    }
+
+}
